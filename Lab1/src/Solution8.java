c5be06af6db6972d961550a288ad8f3afbebd69b
sfrazier7 <sfrazier793@gmail.com>
2022-03-24 23:49:39 -0400

    finished solution 8 code, need to test on Xpanxion Machine

--- Lab1/src/Solution8.java
+++ Lab1/src/Solution8.java
@@ -1,15 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 
 public class Solution8 {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		
 		//Reads The file
+		FileReader fr = new FileReader(new File("C:\\Users\\Owner\\Documents\\the_prince_quotes.txt"));
+		
 		//Convert The file into String
-		//Use the String Split Method to Seperate strings into singular arrays and indexes 
-		//for loop that will run the length of the string array and set each word it encounters to a key
-		//nested for loop that will check string equivalence, if it does int pair value goes up, if it doesn't
-		//word gets added as a new key
+		BufferedReader br = new BufferedReader(fr);
+		
+		//Use the String Split Method to Seperate strings into singular arrays and indexes
+		String[] fileWordArray = br.readLine().split(" ");
+		HashMap<String, Integer> wordCounter = new HashMap<String, Integer>();
+		
+		
+		Integer count = 1;
+		for(int i = 0; i < fileWordArray.length; i++) {
+			//word gets added as a new key if it isn't found
+			if(!wordCounter.containsKey(fileWordArray[i])) {
+				wordCounter.put(fileWordArray[i], count);
+			} else {
+				//if found integer pair value goes up
+				wordCounter.replace(fileWordArray[i], count, count++);
+			}
+		}
 		//HashMap Values Get Printed
+		System.out.println(wordCounter);
 	}
-
 }
