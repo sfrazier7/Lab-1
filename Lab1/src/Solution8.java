50c475f845a64bd864a83d63cfb2e2ddcb1a4128
sfrazier7 <sfrazier793@gmail.com>
2022-03-25 09:42:13 -0400

    Code can properly read one line of code. Will continue testing

--- Lab1/src/Solution8.java
+++ Lab1/src/Solution8.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -10,24 +11,25 @@ public class Solution8 {
 	public static void main(String[] args) throws IOException {
 		
 		//Reads The file
-		FileReader fr = new FileReader(new File("C:\\Users\\Owner\\Documents\\the_prince_quotes.txt"));
+		FileReader fr = new FileReader(new File("C:\\Users\\ShafirFrazier\\OneDrive - Xpanxion\\Documents\\the_prince_quotes.txt"));
 		
 		//Convert The file into String
 		BufferedReader br = new BufferedReader(fr);
 		
 		//Use the String Split Method to Seperate strings into singular arrays and indexes
 		String[] fileWordArray = br.readLine().split(" ");
+		System.out.println(Arrays.toString(fileWordArray));
 		HashMap<String, Integer> wordCounter = new HashMap<String, Integer>();
 		
 		
-		Integer count = 1;
+		Integer count;
 		for(int i = 0; i < fileWordArray.length; i++) {
 			//word gets added as a new key if it isn't found
-			if(!wordCounter.containsKey(fileWordArray[i])) {
-				wordCounter.put(fileWordArray[i], count);
+			if(!(wordCounter.containsKey(fileWordArray[i].toLowerCase()))) {
+				wordCounter.put(fileWordArray[i], 1);
 			} else {
-				//if found integer pair value goes up
-				wordCounter.replace(fileWordArray[i], count, count++);
+				count = wordCounter.get(fileWordArray[i]);
+				wordCounter.replace(fileWordArray[i], count, count+1);
 			}
 		}
 		//HashMap Values Get Printed
